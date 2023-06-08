e88e7d515c5d4f33bd06c10205c339504f60e5b5
divesh-mishra <135318985+divesh-mishra@users.noreply.github.com>
2023-06-08 23:09:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+import pygame
+from sys import exit
+from random import randint, choice
 
+pygame.init()
+
+# Creating Game Window
+screen = pygame.display.set_mode((800, 600))
+
+# Caption and Icon
+pygame.display.set_caption("NINJA ARROWMASTER")
+iconImg = pygame.image.load('ninja.png')
+pygame.display.set_icon(iconImg)
+
+background = pygame.image.load('Assets/sky.png').convert()
+ground = pygame.image.load('Assets/ground.png').convert()
