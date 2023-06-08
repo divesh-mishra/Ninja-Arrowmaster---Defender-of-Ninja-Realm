4a81d92b0a54f1cff82ddcb69e6502a3e655bcc1
AkshayJadhav96 <127526928+AkshayJadhav96@users.noreply.github.com>
2023-06-08 23:14:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -38,3 +38,109 @@ score_font = pygame.font.Font('Assets/ShortBaby-Mg2w.ttf', 40)
 def score_board():
     score_surf = score_font.render(f'Score:{score_var}', True, 32)
     screen.blit(score_surf, (370, 10))
+    
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        player_1 = pygame.image.load('Game/Walking/pw_1.png').convert_alpha()
+        player_2 = pygame.image.load('Game/Walking/pw_2.png').convert_alpha()
+        player_3 = pygame.image.load('Game/Walking/pw_3.png').convert_alpha()
+        player_4 = pygame.image.load('Game/Walking/pw_4.png').convert_alpha()
+        player_5 = pygame.image.load('Game/Walking/pw_5.png').convert_alpha()
+        player_6 = pygame.image.load('Game/Walking/pw_6.png').convert_alpha()
+        player_7 = pygame.image.load('Game/Walking/pw_7.png').convert_alpha()
+        player_8 = pygame.image.load('Game/Walking/pw_8.png').convert_alpha()
+
+        self.player_walk = [player_1, player_2, player_3, player_4, player_5, player_6, player_7, player_8]
+        self.player_walk_index = 0
+
+        player_b1 = pygame.image.load('Game/Shooting/Bow4.png').convert_alpha()
+        player_b2 = pygame.image.load('Game/Shooting/Bow5.png').convert_alpha()
+        player_b3 = pygame.image.load('Game/Shooting/Bow6.png').convert_alpha()
+        player_b4 = pygame.image.load('Game/Shooting/Bow7.png').convert_alpha()
+
+        self.player_bow = [player_b1, player_b2, player_b3, player_b4]
+        self.player_bow_index = 0
+
+        self.jump = pygame.image.load('Game/Walking/pw_1.png').convert_alpha()
+        self.image = self.player_walk[self.player_walk_index]
+
+        self.rect = self.image.get_rect(midbottom=(100, 415))
+        self.gravity = 0
+
+        self.instantaneous_health = 200
+        self.max_health = 1200
+        self.health_bar_length = 300
+        self.heath_bar_ratio = 4
+
+    def player_jump(self):
+        key = pygame.key.get_pressed()
+        if key[pygame.K_SPACE] and self.rect.bottom >= 415:
+            self.gravity = -25
+
+    def act_gravity(self):
+        global arrow_pos_y
+        self.gravity += 1
+        self.rect.bottom += self.gravity
+        arrow_pos_y = self.rect.centery
+
+        if self.rect.bottom >= 415:
+            self.rect.bottom = 415
+            self.gravity = 0
+
+    def player_anim(self):
+        key = pygame.key.get_pressed()
+        if key[pygame.K_RIGHT]:
+            self.player_bow_index += 0.1
+            if self.player_bow_index >= len(self.player_bow):
+                self.player_bow_index = 0
+            self.image = self.player_bow[int(self.player_bow_index)]
+
+        else:
+            if self.rect.bottom < 300:
+                self.image = self.jump
+
+            else:
+                self.player_walk_index += 0.1
+                if self.player_walk_index >= len(self.player_walk):
+                    self.player_walk_index = 0
+                self.image = self.player_walk[int(self.player_walk_index)]
+
+    def generate_arrow(self):
+
+        return Arrow(self.rect.centerx, arrow_pos_y)
+
+    def health_decrease(self, amt):
+        if self.instantaneous_health > 0:
+            self.instantaneous_health -= amt
+        if self.instantaneous_health < 0:
+            self.instantaneous_health = 0
+        pygame.display.update()
+
+    def draw_health_bar(self):
+        # colour, (x,y,width,height), margin width
+        heart_img = pygame.image.load('Assets/heartImg.png').convert_alpha()
+        heart_img_rect = heart_img.get_rect(topright=(30, 16))
+        screen.blit(heart_img, heart_img_rect)
+        pygame.draw.rect(screen, 'Red', (30, 20, self.health_bar_length, 15))
+        pygame.draw.rect(screen, 'Green', (30, 20, self.instantaneous_health / self.heath_bar_ratio, 15))
+
+    def update(self):
+        global score_var
+        global game_on
+        global frames
+        self.player_jump()
+        self.act_gravity()
+        self.player_anim()
+        # self.score_board()
+        self.draw_health_bar()
+        if self.instantaneous_health == 0:
+            # frames = 0
+            score_values_list.append(score_var)
+            self.instantaneous_health = 1200
+            game_on = 3
+
+
+player = Player()
+player_group = pygame.sprite.GroupSingle()
+player_group.add(Player())    
